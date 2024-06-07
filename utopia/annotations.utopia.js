bd600f4b7cb227fe7170fa4505210c31c526aee0
Balazs Bajorics <2226774+balazsbajorics@users.noreply.github.com>
2024-06-07 14:56:46 +0200

    Update annotations.utopia.js

--- utopia/annotations.utopia.js
+++ utopia/annotations.utopia.js
@@ -1,42 +1,47 @@
 import { PromiseCard } from '../app/routes/_index/sections/our-promise'
 
 /**
+ * @type {import("utopia-api").ComponentToRegister}}}
+ */
+const PromiseCardAnnotation = {
+  component: PromiseCard,
+  properties: {
+    heading: {
+      control: 'string-input',
+    },
+    description: {
+      control: 'string-input',
+    },
+    color: {
+      control: 'radio',
+      options: [
+        { label: 'Gray', value: 'gray' },
+        { label: 'Black', value: 'black' },
+        { label: 'Accent', value: 'accent' },
+      ],
+    },
+    icon: {
+      control: 'popuplist',
+      options: [
+        { label: 'Guarantee', value: 'guarantee' },
+        { label: 'Trial', value: 'trial' },
+        { label: 'Shipping', value: 'shipping' },
+      ],
+    },
+    borderRadius: {
+      control: 'number-input',
+      min: 0,
+      max: 100,
+    },
+  },
+}
+
+/**
  * @type {{[modulePath: string]: {[componentName:string]: import("utopia-api").ComponentToRegister}}}
  */
 const Components = {
   '/app/routes/_index/sections/our-promise': {
-    PromiseCard: {
-      component: PromiseCard,
-      properties: {
-        heading: {
-          control: 'string-input',
-        },
-        description: {
-          control: 'string-input',
-        },
-        color: {
-          control: 'radio',
-          options: [
-            { label: 'Gray', value: 'gray' },
-            { label: 'Black', value: 'black' },
-            { label: 'Accent', value: 'accent' },
-          ],
-        },
-        icon: {
-          control: 'popuplist',
-          options: [
-            { label: 'Guarantee', value: 'guarantee' },
-            { label: 'Trial', value: 'trial' },
-            { label: 'Shipping', value: 'shipping' },
-          ],
-        },
-        borderRadius: {
-          control: 'number-input',
-          min: 0,
-          max: 100,
-        },
-      },
-    },
+    PromiseCard: PromiseCardAnnotation,
   },
 }
 
