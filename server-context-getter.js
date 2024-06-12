4249170075f0bdab66fbde3fe606e3cb0a17c9ea
Balint Gabor <127662+gbalint@users.noreply.github.com>
2024-06-12 13:42:57 +0200

    Logging CartID for Utopia

--- server-context-getter.js
+++ server-context-getter.js
@@ -4,37 +4,42 @@ import {
   createCartHandler,
   createCustomerAccountClient,
   createStorefrontClient,
-} from '@shopify/hydrogen';
-import {getStorefrontHeaders} from '@shopify/remix-oxygen';
-import {CART_QUERY_FRAGMENT} from '~/lib/fragments';
-import {AppSession} from '~/lib/session';
+} from '@shopify/hydrogen'
+import { getStorefrontHeaders } from '@shopify/remix-oxygen'
+import { CART_QUERY_FRAGMENT } from '~/lib/fragments'
+import { AppSession } from '~/lib/session'
 
-import * as Hydrogen from '@shopify/hydrogen';
+import * as Hydrogen from '@shopify/hydrogen'
 
-console.log('Hydrogen', Hydrogen);
+console.log('Hydrogen', Hydrogen)
 
 /** The getLoadContext is used both in server.js, and by Utopia's storyboard.js */
 export const getLoadContext =
-  (env, executionContext, mockCartID) => async (request) => {
-    const waitUntil = executionContext.waitUntil.bind(executionContext);
+  (env, executionContext, mockCartID) =>
+  async (request) => {
+    const waitUntil = executionContext.waitUntil.bind(
+      executionContext,
+    )
     const [cache, session] = await Promise.all([
       caches.open('hydrogen'),
       AppSession.init(request, [env.SESSION_SECRET]),
-    ]);
+    ])
 
     /**
      * Create Hydrogen's Storefront client.
      */
-    const {storefront} = createStorefrontClient({
+    const { storefront } = createStorefrontClient({
       cache,
       waitUntil,
-      i18n: {language: 'EN', country: 'US'},
-      publicStorefrontToken: env.PUBLIC_STOREFRONT_API_TOKEN,
-      privateStorefrontToken: env.PRIVATE_STOREFRONT_API_TOKEN,
+      i18n: { language: 'EN', country: 'US' },
+      publicStorefrontToken:
+        env.PUBLIC_STOREFRONT_API_TOKEN,
+      privateStorefrontToken:
+        env.PRIVATE_STOREFRONT_API_TOKEN,
       storeDomain: env.PUBLIC_STORE_DOMAIN,
       storefrontId: env.PUBLIC_STOREFRONT_ID,
       storefrontHeaders: getStorefrontHeaders(request),
-    });
+    })
 
     // /**
     //  * Create a client for Customer Account API.
@@ -52,17 +57,24 @@ export const getLoadContext =
      * create and update the cart in the session.
      */
 
-    const IS_TEST_ENVIRONMENT = typeof window !== 'undefined';
+    const cartId =
+      mockCartID != null
+        ? () => mockCartID
+        : cartGetIdDefault(request.headers)
+
+    console.log('Logging CartID for Utopia', cartId)
+
+    const IS_TEST_ENVIRONMENT =
+      typeof window !== 'undefined'
 
     const cart = createCartHandler({
       storefront,
-      getCartId:
-        mockCartID != null
-          ? () => mockCartID
-          : cartGetIdDefault(request.headers),
-      setCartId: IS_TEST_ENVIRONMENT ? () => {} : cartSetIdDefault(),
+      getCartId: cartId,
+      setCartId: IS_TEST_ENVIRONMENT
+        ? () => {}
+        : cartSetIdDefault(),
       cartQueryFragment: CART_QUERY_FRAGMENT,
-    });
+    })
 
     return {
       session,
@@ -71,5 +83,5 @@ export const getLoadContext =
       cart,
       env,
       waitUntil,
-    };
-  };
+    }
+  }
