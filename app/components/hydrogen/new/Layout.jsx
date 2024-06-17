d74fc4dcba92faeed4986b85225d6d56d6c0a4c1
Balint Gabor <127662+gbalint@users.noreply.github.com>
2024-06-17 11:37:40 +0200

    Make direction prop really work

--- app/components/hydrogen/new/Layout.jsx
+++ app/components/hydrogen/new/Layout.jsx
@@ -254,6 +254,7 @@ export const Column = forwardRef(
       maxHeight,
       maxWidth,
       padded,
+      direction = 'down',
       className,
       ...props
     },
@@ -271,13 +272,13 @@ export const Column = forwardRef(
           resizeX,
           resizeY,
           wrap,
+          direction,
           className,
         })}
         data-h2='Column'
         {...props}
         style={{
           ...props.style,
-          flexDirection: 'column',
           padding: padded ? 100 : undefined,
           maxWidth: maxWidth ?? props.style?.maxWidth,
           maxHeight: maxHeight ?? props.style?.maxHeight,
@@ -300,6 +301,7 @@ export const Row = forwardRef(
       maxHeight,
       maxWidth,
       padded,
+      direction = 'right',
       className,
       ...props
     },
@@ -317,13 +319,13 @@ export const Row = forwardRef(
           resizeX,
           resizeY,
           wrap,
+          direction,
           className,
         })}
         data-h2='Column'
         {...props}
         style={{
           ...props.style,
-          flexDirection: 'row',
           padding: padded ? 100 : undefined,
           maxWidth: maxWidth ?? props.style?.maxWidth,
           maxHeight: maxHeight ?? props.style?.maxHeight,
