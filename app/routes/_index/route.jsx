ccad5bb939343f655e8394790f0f5a1e49d1996f
Balazs Bajorics <2226774+balazsbajorics@users.noreply.github.com>
2024-10-04 13:28:07 +0200

    adding custom js to the Homepage route component to demonstrate that this breaks inside Utopia

--- app/routes/_index/route.jsx
+++ app/routes/_index/route.jsx
@@ -7,6 +7,26 @@ import { useLogRerender } from '~/helpers/logging'
 export default function Homepage() {
   useLogRerender('Homepage')
 
+  // this seems to break inside Utopia:
+  const [ID] = React.useState(
+    Math.random().toString(36).substring(2, 6),
+  )
+  const renderCount = React.useRef(0)
+  renderCount.current = renderCount.current + 1
+  console.log(
+    `root route ${ID} render ${renderCount.current}`,
+  )
+  React.useEffect(() => {
+    console.log(
+      `root route ${ID} mount ${renderCount.current}`,
+    )
+    return () => {
+      console.log(
+        `root route ${ID} unmount ${renderCount.current}`,
+      )
+    }
+  }, [])
+
   return (
     <Column>
       <Hero />
