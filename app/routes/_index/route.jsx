1b8c4f93fe5a8daf44f37479924f42233f02f690
Balazs Bajorics <2226774+balazsbajorics@users.noreply.github.com>
2024-10-04 12:50:25 +0200

    adding a bunch of rerender logging

--- app/routes/_index/route.jsx
+++ app/routes/_index/route.jsx
@@ -1,7 +1,45 @@
+import React from 'react'
+
 import { Column } from '@h2/new/Layout'
 import Hero from './sections/hero'
 
 export default function Homepage() {
+  const renderCount = React.useRef(Math.random())
+  renderCount.current = renderCount.current + 1
+  console.log('Homepage render', renderCount.current)
+
+  if (
+    typeof performance !== 'undefined' &&
+    typeof performance.measure !== 'undefined'
+  ) {
+    performance.measure(
+      `Homepage route render ${renderCount.current}`,
+      {
+        start: performance.now(),
+        duration: 50,
+      },
+    )
+  }
+
+  React.useEffect(() => {
+    return function cleanup() {
+      // we are about to unmount!
+      console.log('Homepage unmount', renderCount.current)
+      if (
+        typeof performance !== 'undefined' &&
+        typeof performance.measure !== 'undefined'
+      ) {
+        performance.measure(
+          `Homepage route unmount ${renderCount.current}`,
+          {
+            start: performance.now(),
+            duration: 50,
+          },
+        )
+      }
+    }
+  }, [])
+
   return (
     <Column>
       <Hero />
--- app/routes/_index/sections/hero.jsx
+++ app/routes/_index/sections/hero.jsx
@@ -1,6 +1,42 @@
 import * as React from 'react'
 
 export default function Hero() {
+  const renderCount = React.useRef(Math.random())
+  renderCount.current = renderCount.current + 1
+  console.log('Hero render', renderCount.current)
+
+  if (
+    typeof performance !== 'undefined' &&
+    typeof performance.measure !== 'undefined'
+  ) {
+    performance.measure(
+      `Hero render ${renderCount.current}`,
+      {
+        start: performance.now(),
+        duration: 50,
+      },
+    )
+  }
+
+  React.useEffect(() => {
+    return function cleanup() {
+      // we are about to unmount!
+      console.log('Hero unmount', renderCount.current)
+      if (
+        typeof performance !== 'undefined' &&
+        typeof performance.measure !== 'undefined'
+      ) {
+        performance.measure(
+          `Hero unmount ${renderCount.current}`,
+          {
+            start: performance.now(),
+            duration: 50,
+          },
+        )
+      }
+    }
+  }, [])
+
   return (
     <section className='w-full relative min-h-8 h-svh max-h-[64rem] bg-darkGray text-white'>
       <div className='min-h-inherit h-full mx-auto px-4 md:px-8 lg:px-10 w-full relative z-10 max-w-7xl pb-24 pt-36'>
@@ -40,6 +76,41 @@ function Counter() {
   const renderCount = React.useRef(0)
   renderCount.current += 1
 
+  console.log('Counter render', renderCount.current)
+
+  if (
+    typeof performance !== 'undefined' &&
+    typeof performance.measure !== 'undefined'
+  ) {
+    performance.measure(
+      `Counter render ${renderCount.current}`,
+      {
+        start: performance.now(),
+        duration: 50,
+      },
+    )
+  }
+
+  React.useEffect(() => {
+    return function cleanup() {
+      // we are about to unmount!
+      console.log('Counter unmount', renderCount.current)
+
+      if (
+        typeof performance !== 'undefined' &&
+        typeof performance.measure !== 'undefined'
+      ) {
+        performance.measure(
+          `Counter unmount ${renderCount.current}`,
+          {
+            start: performance.now(),
+            duration: 50,
+          },
+        )
+      }
+    }
+  }, [])
+
   return (
     <div style={{ contain: 'layout' }}>
       <div>Render count:{renderCount.current}</div>
@@ -57,7 +128,6 @@ function Counter() {
         style={{
           cursor: 'pointer',
           backgroundColor: '#aaaaaa33',
-          position: 'absolute',
           left: 26,
           top: 45,
           width: 134,
